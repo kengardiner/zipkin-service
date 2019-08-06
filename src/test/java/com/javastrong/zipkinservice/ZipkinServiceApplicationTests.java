4733c70d2e3cff07c4e87de2b51e2d12a2341c03
Ken Gardiner <kagemail@gmail.com>
2019-08-05 20:32:12 -0400

    Initial commit

---
+++ src/test/java/com/javastrong/zipkinservice/ZipkinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javastrong.zipkinservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
